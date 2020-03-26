b1649f80cf13864462b4afba15b7154856122d18
1 <1>
2020-03-26 11:22:52 -0400

    New submission

---
+++ env/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/Users/reed/Downloads/django-forms-builder-master/env/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==2.1.15','django-admin.py'
+__requires__ = 'Django==2.1.15'
+__import__('pkg_resources').run_script('Django==2.1.15', 'django-admin.py')
